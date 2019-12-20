1d4ad6dc57066f8eada005c225359c35b90fb354
Sergei Lavrov <sergei.lavrov@jibrel.network>
2019-12-20 13:39:45 +0300

    Linter fixes

--- jibrel/kyc/views.py
+++ jibrel/kyc/views.py
@@ -1,6 +1,4 @@
-from rest_framework import (
-    mixins
-)
+from rest_framework import mixins
 from rest_framework.generics import GenericAPIView
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import (
