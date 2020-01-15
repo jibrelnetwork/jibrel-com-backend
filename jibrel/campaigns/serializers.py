a1e166f0186ab564b521285e8d07c83570b557c9
fu2re <fu2re@yandex.ru>
2020-01-16 00:07:34 +0400

    camelCase at api

--- jibrel/campaigns/serializers.py
+++ jibrel/campaigns/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 
 from jibrel.campaigns.models import (
@@ -17,14 +18,16 @@ class CompanySerializer(serializers.ModelSerializer):
 
 class SecuritySerializer(serializers.ModelSerializer):
     company = CompanySerializer()
+    createdAt = serializers.DateTimeField(source='created_at')
+    updatedAt = serializers.DateTimeField(source='updated_at')
 
     class Meta:
         model = Security
         fields = (
             'uuid',
             'type',
-            'created_at',
-            'updated_at',
+            'createdAt',
+            'updatedAt',
             'company'
         )
 
@@ -32,18 +35,26 @@ class SecuritySerializer(serializers.ModelSerializer):
 class OfferingSerializer(serializers.ModelSerializer):
     security = SecuritySerializer()
     equity = serializers.SerializerMethodField()
+    limitMinAmount = serializers.DecimalField(
+        max_digits=settings.ACCOUNTING_MAX_DIGITS, decimal_places=2, source='limit_min_amount')
+    limitMaxAmount = serializers.DecimalField(
+        max_digits=settings.ACCOUNTING_MAX_DIGITS, decimal_places=2, source='limit_min_amount')
+    dateStart = serializers.DateTimeField(source='date_start')
+    dateEnd = serializers.DateTimeField(source='date_end')
+    createdAt = serializers.DateTimeField(source='created_at')
+    updatedAt = serializers.DateTimeField(source='updated_at')
 
     class Meta:
         model = Offering
         fields = (
             'uuid',
             'security',
-            'limit_min_amount',
-            'limit_max_amount',
-            'date_start',
-            'date_end',
-            'created_at',
-            'updated_at',
+            'limitMinAmount',
+            'limitMaxAmount',
+            'dateStart',
+            'dateEnd',
+            'createdAt',
+            'updatedAt',
             'valuation',
             'goal',
             'round',
