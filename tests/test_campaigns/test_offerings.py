0d5996e3e56e3fb289ef43d769130ca8614839f3
fu2re <fu2re@yandex.ru>
2020-01-15 22:27:02 +0400

    test simplified

--- tests/test_campaigns/test_offerings.py
+++ tests/test_campaigns/test_offerings.py
@@ -1,20 +1,15 @@
 import pytest
-from rest_framework.exceptions import NotAuthenticated
 
 from tests.test_payments.utils import validate_response_schema
 
 
-def test_auth(client, mocker):
-    def handle_exc(self, exc):
-        raise exc
-    mocker.patch('jibrel.campaigns.views.CMSOfferingsAPIView.handle_exception', handle_exc)
-    with pytest.raises(NotAuthenticated):
-        client.get(f'/v1/campaigns/company/random/offerings')
-
-
 @pytest.mark.django_db
-def test_view(client, full_verified_user, security, offering_factory, mocker):
+def test_auth(client, full_verified_user):
+    url = '/v1/campaigns/company/random/offerings'
+    response = client.get(url)
+    assert response.status_code == 403
+
     client.force_login(full_verified_user)
-    response = client.get(f'/v1/campaigns/company/{security.company.slug}/offerings')
+    response = client.get(f'/v1/campaigns/company/random/offerings')
     assert response.status_code == 200
     validate_response_schema('/v1/campaigns/company/{companySlug}/offerings', 'GET', response)
