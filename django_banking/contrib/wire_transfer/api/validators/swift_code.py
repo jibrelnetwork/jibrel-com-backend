eaf75a05d2619cb92ca933784b72567561ccf54a
Sergei Lavrov <sergei.lavrov@jibrel.network>
2020-01-21 15:03:28 +0300

    django_banking: extracted swift validation function

--- django_banking/contrib/wire_transfer/api/validators/swift_code.py
+++ django_banking/contrib/wire_transfer/api/validators/swift_code.py
@@ -11,8 +11,12 @@ def get_swift_country_code(swift_number):
     return swift_number[4:6].upper()
 
 
+def is_valid_swift_code(value):
+    return swift_regexp.fullmatch(value) is not None
+
+
 def swift_code_validator(value):
-    if swift_regexp.fullmatch(value) is None:
+    if not is_valid_swift_code(value):
         raise ValidationError("Invalid SWIFT/BIC number")
 
     country_code = get_swift_country_code(value)
