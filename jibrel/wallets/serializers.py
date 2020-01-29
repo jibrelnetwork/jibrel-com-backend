778f393e32b19a286b4f820368510aae7266b702
Yuriy Homyakov <yuriy.homyakov@gmail.com>
2020-01-29 11:33:39 +0300

    fix submodule

--- jibrel/wallets/serializers.py
+++ jibrel/wallets/serializers.py
@@ -8,11 +8,6 @@ class WalletSerializer(serializers.ModelSerializer):
         model = Wallet
         exclude = ['user', 'address', 'deleted']
         read_only_fields = ['version_number']
-    #
-    # def validate_uid(self, value):
-    #     if self.instance and self.instance.uid != value:
-    #         raise serializers.ValidationError("Can't change Wallet UID")
-    #     return value
 
 
 class WalletUpdateSerializer(serializers.ModelSerializer):
