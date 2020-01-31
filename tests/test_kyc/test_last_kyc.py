d17c6ff19add397cc7f0cf4fd553fd8675a1b4a0
fu2re <fu2re@yandex.ru>
2020-01-31 16:16:20 +0400

    jcback-54 test last_kyc

--- tests/test_kyc/test_last_kyc.py
+++ tests/test_kyc/test_last_kyc.py
@@ -1,6 +1,7 @@
 import pytest
 
-from jibrel.kyc.models import BaseKYCSubmission
+from jibrel.authentication.factories import ApprovedIndividualKYCFactory
+from jibrel.kyc.models import BaseKYCSubmission, IndividualKYCSubmission
 
 
 @pytest.mark.django_db
@@ -64,3 +65,62 @@ def test_approved_kyc_orgaziational(
     assert response.data['companyAddressRegistered']['city'] == company_address_registered.city
     assert response.data['companyAddressRegistered']['postCode'] == company_address_registered.post_code
     assert response.data['companyAddressRegistered']['country'] == company_address_registered.country
+
+@pytest.mark.django_db
+def test_kyc_ordering(
+    client,
+    full_verified_user,
+):
+    profile = full_verified_user.profile
+    last_kyc = full_verified_user.profile.last_kyc.details
+    submissions = [ApprovedIndividualKYCFactory.create(
+        profile=profile,
+        passport_document__profile=profile,
+        proof_of_address_document__profile=profile,
+        status=IndividualKYCSubmission.PENDING
+    ) for _i in range(5)]
+    profile.refresh_from_db()
+    assert submissions[0].status == IndividualKYCSubmission.PENDING
+    assert profile.last_kyc.pk == last_kyc.pk
+
+    # approve another kyc. As soon as it created later - it became active
+    last_kyc.reject()
+    profile.refresh_from_db()
+    assert profile.last_kyc is None
+
+    last_kyc.approve()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == last_kyc.pk
+
+    # approve another kyc. As soon as it created later - it became active
+    submissions[0].approve()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == submissions[0].base_kyc_id
+
+    submissions[3].approve()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == submissions[3].base_kyc_id
+
+    # approve another kyc. But the latest one is still active
+    submissions[2].approve()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == submissions[3].base_kyc_id
+
+    # reject some kyc
+    last_kyc.reject()
+    submissions[1].reject()
+    submissions[4].reject()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == submissions[3].base_kyc_id
+
+    # test clone and reject
+    one_more_kyc = last_kyc.clone()
+    one_more_kyc.reject()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == submissions[3].base_kyc_id
+
+    # text clone. check if it set as active
+    another_kyc = last_kyc.clone()
+    another_kyc.approve()
+    profile.refresh_from_db()
+    assert profile.last_kyc.pk == another_kyc.base_kyc_id
