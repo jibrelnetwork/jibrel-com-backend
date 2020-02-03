1363e45a3f9a714b2c916445a5ba1ea6eed17ab6
fu2re <fu2re@yandex.ru>
2020-02-03 23:12:51 +0400

    rework ott verify method

--- jibrel/authentication/token_generator.py
+++ jibrel/authentication/token_generator.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from django.conf import settings
 from django.db import transaction
-from django.db.models import Q
+from django.db.models import F
 from django.db.models.functions import Now
 
 from jibrel.authentication.models import (
@@ -17,7 +17,7 @@ VERIFY_EMAIL_TOKEN_LIFETIME = settings.VERIFY_EMAIL_TOKEN_LIFETIME
 FORGOT_PASSWORD_EMAIL_TOKEN_LIFETIME = settings.FORGOT_PASSWORD_EMAIL_TOKEN_LIFETIME
 
 
-class TokenGenerator:
+class TokenGenerator(object):
     def __init__(self, lifetime: int, operation_type: int):
         self.lifetime = lifetime
         self.operation_type = operation_type
@@ -37,20 +37,21 @@ class TokenGenerator:
         return token_obj.token
 
     @transaction.atomic()
-    def validate(self, token: uuid.UUID) -> Optional[User]:
+    def validate(self, token: uuid.UUID) -> Optional[OneTimeToken]:
         try:
-            token_obj = OneTimeToken.objects.get(
-                Q(checked=False) | Q(operation_type=OneTimeToken.EMAIL_VERIFICATION),
+            token_obj = OneTimeToken.objects.filter(
                 token=token,
                 operation_type=self.operation_type,
                 created_at__gte=Now() - timedelta(seconds=self.lifetime),
-            )
+            ).annotate(
+                checked_already=F('checked')
+            ).get()
         except OneTimeToken.DoesNotExist:
             return None
         if not token_obj.checked:
             token_obj.checked = True
             token_obj.save()
-        return token_obj.user
+        return token_obj
 
 
 verify_token_generator = TokenGenerator(
@@ -66,9 +67,4 @@ activate_reset_password_token_generator = TokenGenerator(
 complete_reset_password_token_generator = TokenGenerator(
     lifetime=FORGOT_PASSWORD_EMAIL_TOKEN_LIFETIME,
     operation_type=OneTimeToken.PASSWORD_RESET_COMPLETE
-)
-
-deposit_confirmation_token_generator = TokenGenerator(
-    lifetime=3600,
-    operation_type=OneTimeToken.CRYPTO_WITHDRAWAL_CONFIRMATION
 )
