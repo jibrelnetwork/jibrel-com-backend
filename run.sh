5a6b38e4c9e23594ec386e33320e2297a89a3a09
Ilya Kharlamov <ilya.kharlamov@jibrel.network>
2020-02-14 16:54:08 +0300

    Fix quotes

--- run.sh
+++ run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 
-dockerize -timeout 1m -wait tcp://${MAIN_DB_HOST:-main_db}:${MAIN_DB_PORT:-5432}
-dockerize -timeout 1m -wait tcp://`python -c 'import os; p = os.getenv("CELERY_BROKER_URL", "").split("//", 1)[-1].split("@")[1]; print(p)'`
-
+dockerize -timeout 1m -wait tcp://"${MAIN_DB_HOST:-main_db}":"${MAIN_DB_PORT:-5432}"
+dockerize -timeout 1m -wait tcp://"$(python -c 'import os; p = os.getenv("CELERY_BROKER_URL", "").split("//", 1)[-1].split("@")[1]; print(p)')"
 
 if [[ "$1" = "api" ]]; then
-    echo "Starting jibrel.com backend service in '${ENVIRONMENT}' environment on node `hostname`"
+    echo "Starting jibrel.com backend service in '${ENVIRONMENT}' environment on node $(hostname)"
     python manage.py check
     python manage.py migrate --noinput
     if [ "${ENVIRONMENT}" = "production" ]; then
         gunicorn jibrel.wsgi \
           -w 4 \
-          -b 0.0.0.0:${PORT} \
+          -b 0.0.0.0:"${PORT}" \
           --log-file=- \
-          --log-level ${LOG_LEVEL}
+          --log-level "${LOG_LEVEL}"
     else
-        python /app/manage.py runserver 0.0.0.0:${PORT}
+        python /app/manage.py runserver 0.0.0.0:"${PORT}"
     fi
 
 elif [[ "$1" = "admin" ]]; then
-    echo "Starting jibrel.com admin service in '${ENVIRONMENT}' environment on node `hostname`"
+    echo "Starting jibrel.com admin service in '${ENVIRONMENT}' environment on node $(hostname)"
     python manage.py check --settings jibrel_admin.settings
     python manage.py migrate --noinput --settings jibrel_admin.settings
     {
@@ -36,20 +35,20 @@ elif [[ "$1" = "admin" ]]; then
         python manage.py collectstatic --noinput --settings jibrel_admin.settings
         gunicorn jibrel_admin.wsgi \
           -w 4 \
-          -b 0.0.0.0:${PORT} \
+          -b 0.0.0.0:"${PORT}" \
           --log-file=- \
-          --log-level ${LOG_LEVEL}
+          --log-level "${LOG_LEVEL}"
     else
-        python /app/manage.py runserver 0.0.0.0:${PORT} --settings jibrel_admin.settings
+        python /app/manage.py runserver 0.0.0.0:"${PORT}" --settings jibrel_admin.settings
     fi
 
 elif [[ "$1" = "celeryworker" ]]; then
     echo "Starting jibrel.com worker"
-    celery -A jibrel worker -l ${LOG_LEVEL} ${@:13}
+    celery -A jibrel worker -l "${LOG_LEVEL}" "${@:13}"
 
 elif [[ "$1" = "celerybeat" ]]; then
     echo "Starting jibrel.com celery beat"
-    celery -A jibrel beat -l ${LOG_LEVEL}
+    celery -A jibrel beat -l "${LOG_LEVEL}"
 
 else
     exec "$@"
