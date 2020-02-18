5b854061a9a33d620e838beab60a9045a0412b71
fu2re <fu2re@yandex.ru>
2020-02-18 14:02:43 +0400

    fix offering addition

--- jibrel/core/common/helpers.py
+++ jibrel/core/common/helpers.py
@@ -17,3 +17,14 @@ def get_from_qs(method):
             return getattr(obj, name)
         return method(obj, *args, **kwargs)
     return wrapper
+
+
+def default_value_for_new_object(value):
+    def decorator(method):
+        @functools.wraps(method)
+        def wrapper(obj, *args, **kwargs):
+            if getattr(obj._state, 'adding', True):
+                return value
+            return method(obj, *args, **kwargs)
+        return wrapper
+    return decorator
