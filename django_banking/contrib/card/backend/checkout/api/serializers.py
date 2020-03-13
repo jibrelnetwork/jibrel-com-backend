2be75d496d9497c2ae4f798e2223fb03ac4279fe
Sergei Lavrov <sergei.lavrov@jibrel.network>
2020-03-13 13:32:42 +0300

    JCBACK-21: Checkout Improved card token validation

--- django_banking/contrib/card/backend/checkout/api/serializers.py
+++ django_banking/contrib/card/backend/checkout/api/serializers.py
@@ -1,18 +1,20 @@
-import re
-
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from django_banking.models import Operation
+from jibrel.core.rest_framework import RegexValidator
 
 
 class CheckoutTokenSerializer(serializers.Serializer):
-    cardToken = serializers.CharField(required=True)
+    cardToken = serializers.CharField(
+        required=True,
+        validators=[RegexValidator(r'^(tok)_(\w{26})$')],
+        error_messages={
+            'invalid': 'Token is not valid',
+        }
+    )
 
     def validate_cardToken(self, value):
         value = value.strip()
-        if not re.match('^(tok)_(\w{26})$', value):
-            raise ValidationError('Bad token')
-        elif Operation.objects.filter(references__checkout_token=value).exists():
-            raise ValidationError('Token used')
+        if Operation.objects.filter(references__checkout_token=value).exists():
+            raise serializers.ValidationError('used', 'Token already used')
         return value
