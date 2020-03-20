9d4a3c23b5b9cb7a985f4e3c15c4da874ae1dfad
fu2re <fu2re@yandex.ru>
2020-03-20 17:08:37 +0400

    application id for deposit

---
+++ jibrel/payments/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from django_banking.api.serializers import (
+    DepositOperationSerializer,
+    ExchangeOperationSerializer,
+    OperationSerializer,
+    WithdrawalOperationSerializer
+)
+from django_banking.models import Operation
+from django_banking.models.transactions.enum import OperationType
+
+
+class InvestmentDepositOperationSerializer(DepositOperationSerializer):
+    investmentApplication = serializers.SerializerMethodField(
+        method_name='get_investment_application'
+    )
+
+    def get_investment_application(self, obj):
+        investment_application = obj.deposited_application.first()
+        if investment_application:
+            return str(investment_application.pk)
+
+    class Meta:
+        model = Operation
+        fields = list(DepositOperationSerializer.Meta.fields) + [
+            'investmentApplication'
+        ]
+
+
+class InvestmentOperationSerializer(OperationSerializer):
+    type_to_serializer = {
+        OperationType.DEPOSIT: InvestmentDepositOperationSerializer(),
+        OperationType.WITHDRAWAL: WithdrawalOperationSerializer(),
+        OperationType.BUY: ExchangeOperationSerializer(),
+        OperationType.SELL: ExchangeOperationSerializer(),
+    }
+
+    def to_representation(self, instance):
+        return self.type_to_serializer[instance.type].to_representation(instance)
